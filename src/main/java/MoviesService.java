0d3e9a4420e780fd23685d813cc4587b7851d0bf
Alen Fekonja <alen.fekonja@student.um.si>
2024-08-10 14:24:52 +0200

    Added and partially implemented MoviesService methods

---
+++ src/main/java/MoviesService.java
@@ -0,0 +1,42 @@
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+@ApplicationScoped
+public class MoviesService {
+
+    @Inject
+    MoviesRepository movieRepository;
+
+    public List<Movies> listAll() {
+        return movieRepository.listAll();
+    }
+
+    public Movies findById(long id) {
+        return movieRepository.findById(id);
+    }
+
+    @Transactional
+    public void addMovie(Movies movie) {
+        movieRepository.persist(movie);
+    }
+
+    @Transactional
+    public void updateMovie(long id, Movies movie) {
+        Movies entity = movieRepository.findById(id);
+        if (entity != null) {
+            entity.setTitle(movie.getTitle());
+            entity.setDescription(movie.getDescription());
+            entity.setYear(movie.getYear());
+            entity.setImages(movie.getImages());
+            movieRepository.persist(entity);
+        }
+    }
+
+    @Transactional
+    public void deleteMovie(long id) {
+        movieRepository.deleteById(id);
+    }
+}
