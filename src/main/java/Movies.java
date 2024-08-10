dc2c50834ca9400eeba8fae1fc768dbbbf2519f7
Alen Fekonja <alen.fekonja@student.um.si>
2024-08-10 13:55:09 +0200

    Added Movies entity

---
+++ src/main/java/Movies.java
@@ -0,0 +1,77 @@
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
+@Entity
+public class Movies {
+
+    @Id
+    @NotBlank
+    private String imdbID;
+
+    @NotBlank
+    private String title;
+
+    private int year;
+
+    private String description;
+
+    @ElementCollection
+    @Lob
+    private List<byte[]> images;
+
+    public Movies() {}
+
+    public Movies(String imdbID, String title, int year, String description, List<byte[]> images) {
+        this.imdbID = imdbID;
+        this.title = title;
+        this.year = year;
+        this.description = description;
+        this.images = images;
+    }
+
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public void setImdbID(String imdbID) {
+        this.imdbID = imdbID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<byte[]> getImages() {
+        return images;
+    }
+
+    public void setImages(List<byte[]> images) {
+        this.images = images;
+    }
+}
+
